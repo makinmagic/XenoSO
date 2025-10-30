6aadee8bc17d35855b87387e84eb9259380b9766
makinmagic <timbityyc@gmail.com>
2025-10-30 16:23:22 -0600

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -45,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // Define emoji rules
 function formatDisplayName(name) {
-  const adminNames = ["Sorta", "Savaki", "Daat", "Xeno", "Eric", "Sneaky", "Nyx", "Bruglar"];
+  const adminNames = ["Sorta", "Savaki", "Daat", "Xeno", "Eric", "Sneaky", "Nyx", "Bruglar", "Breaker"];
   const emojiMap = {
     "Mr Teddy": "🐻"
   };
